ce806481881bfb73749162efece48ea107439cd5
RepleDEV <ragazzochaesa@gmail.com>
2020-08-28 07:43:33 +0700

    stash

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -3,12 +3,10 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function play_splash_screen() {
-    console.log("playing splash screen");
     var file = await fs.readFile("src/index.html", {encoding: "utf-8"});
-    console.log(file, "test...");
-    return file;
+    console.log(file);
 }
 
 (async () => {
-    return await play_splash_screen();
+    // return await play_splash_screen();
 })();
