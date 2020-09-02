ed6bc9866b52c86e06198b29e47f945ccc3e22c9
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-09-02 10:25:52 +0700

    Added skip all animations on window double click. Also added JSDoc

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -2,12 +2,22 @@ const anime = require('animejs');
 
 // Menu animations
 
+// Current playing animations variable
+var current_animations = [];
+
+// Undefined current animation variable
 splash_screen.current_animation;
 
+/**
+ * Play show animation function
+ */
 splash_screen.play_show_animation = function() {
+    // Return a promise
     return new Promise(async (resolve, reject) => {
+        // If targeted element doesn't exist, reject the promise
         if ($("#svg_title").length == 0)return reject("#svg_title DOESN'T EXIST");
 
+        // Update this element's current animation
         this.current_animation = anime({
             targets: '.main #svg_title .svg_title_group path',
             strokeDashoffset: [anime.setDashoffset, 0],
@@ -17,17 +27,33 @@ splash_screen.play_show_animation = function() {
             delay: (el, i) => i * 250 + 200
         });
 
+        // Push this element's current animation to the current_animations variable
+        current_animations.push(this.current_animation);
+
+        // Wait for the animation promise to return
         await this.current_animation.finished;
 
+        // Get current animation's index
+        var current_animation_index = current_animations.indexOf(this.current_animation);
+
+        // Remove the this animation from current_animations variable
+        current_animations.splice(current_animation_index, 1);
+
+        // Set this element's current animation to undefined
         this.current_animation = undefined;
-        resolve("Success");
+
+        // Return the promise
+        return resolve("Success");
     });
 };
 
 splash_screen.play_hide_animation = function() {
+    // Return a promise
     return new Promise(async (resolve, reject) => {
+        // If targeted element doesn't exist, reject the promise
         if ($("#svg_title").length == 0)return reject("#svg_title DOESN'T EXIST");
 
+        // Update this element's current animation
         this.current_animation = anime({
             targets: '.main #svg_title .svg_title_group path',
             opacity:0,
@@ -36,28 +62,60 @@ splash_screen.play_hide_animation = function() {
             delay: (el, i) => i * 150
         });
 
+        // Push this element's current animation to the current_animations variable
+        current_animations.push(this.current_animation);
+
+        // Wait for the animation promise to return
         await this.current_animation.finished;
 
+        // Get current animation's index
+        var current_animation_index = current_animations.indexOf(this.current_animation);
+
+        // Remove the this animation from current_animations variable
+        current_animations.splice(current_animation_index, 1);
+
+        // Set this element's current animation to undefined
         this.current_animation = undefined;
-        resolve("Success");
+
+        // Return the promise
+        return resolve("Success");
     });
 };
 
+/**
+ * Skip animation function
+ */
 splash_screen.skip_animation = function() {
+    // Check if there is actually an animation playing
     if (this.current_animation === undefined)return "No animations are currently playing!";
+
+    // Get the animation
     var anim = this.current_animation;
+
+    // Seek to the animation duration
     anim.seek(anim.duration);
 
+    // Clear this element's current animation
     this.current_animation = undefined;
+
+    // Return
     return "Skipped animation";
 }
 
+
+// Undefined current animation variable
 dots_loading.current_animation;
 
+/**
+ * Play show animation function
+ */
 dots_loading.play_loading_animation = function() {
+    // Return a promise
     return new Promise(async (resolve, reject) => {
+        // If targeted element doesn't exist, reject the promise
         if ($(".loading_container").length == 0)return reject(".loading_container DOESN'T EXIST");
 
+        // Update this element's current animation
         this.current_animation = anime({
             targets: '.loading_container .dot',
             translateY: [
@@ -81,15 +139,32 @@ dots_loading.play_loading_animation = function() {
             delay: (el, i) => i * 50 + 600
         });
 
+        // Push this element's current animation to the current_animations variable
+        current_animations.push(this.current_animation);
+
+        // Wait for the animation promise to return
         await this.current_animation.finished;
 
-        resolve("Success");
+        // Get current animation's index
+        var current_animation_index = current_animations.indexOf(this.current_animation);
+
+        // Remove the this animation from current_animations variable
+        current_animations.splice(current_animation_index, 1);
+
+        // Set this element's current animation to undefined
+        this.current_animation = undefined;
+
+        // Return the promise
+        return resolve("Success");
     });
 }
+
 dots_loading.play_hide_animation = function() {
     return new Promise(async (resolve, reject) => {
+        // If targeted element doesn't exist, reject the promise
         if ($(".loading_container").length == 0)return reject(".loading_container DOESN'T EXIST");
         
+        // Update this element's current animation
         this.current_animation = anime({
             targets: '.loading_container .dot',
             opacity: 0,
@@ -98,10 +173,45 @@ dots_loading.play_hide_animation = function() {
             delay: (el, i) => i * 50,
         });
 
-        this.current_animation.finished;
+        // Push this element's current animation to the current_animations variable
+        current_animations.push(this.current_animation);
 
-        resolve("Success");
+        // Wait for the animation promise to return
+        await this.current_animation.finished;
+
+        // Get current animation's index
+        var current_animation_index = current_animations.indexOf(this.current_animation);
+
+        // Remove the this animation from current_animations variable
+        current_animations.splice(current_animation_index, 1);
+
+        // Set this element's current animation to undefined
+        this.current_animation = undefined;
+
+        // Return the promise
+        return resolve("Success");
     });
 }
 
-// Hover animations
+// Skip animation functions
+
+// If the user double clicks the window
+$(window).dblclick(e => {
+    // Skip animation
+    skip_all_playing_animations();
+})
+
+// Skip all animations
+function skip_all_playing_animations() {
+    // Check if there are any playing animations
+    if (current_animations.length == 0) return "No animations are playing!";
+
+    // Iterate through every animation
+    current_animations.forEach(animation => {
+        // Seek to the animation duration
+        animation.seek(animation.duration);
+    });
+
+    // Return
+    return "Skipped Animations";
+}
